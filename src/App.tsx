ab2d29340d1afa46fe83cb02c6c9e23b2d989dc7
Lucas <lucas.pina@prodepa.pa.gov.br>
2023-01-03 11:52:39 -0300

    inicio config Router e layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
 
 import { defaultTheme } from './styles/themes/deafult'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
