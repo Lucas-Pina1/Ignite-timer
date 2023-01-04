8b730bc197bcc44dee93e5fe3f40c6240956b522
Lucas <lucas.pina@prodepa.pa.gov.br>
2023-01-04 11:37:50 -0300

    estrutura de arquivos, style defaultLayout, styled-componentes header

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout/index.tsx'
 import { History } from './pages/History'
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/index'
 
 export function Router() {
   return (
